8888efc343f681d3503280d970879164d8212f9c
RMAS81 <rmas@masxort.com>
2025-04-03 18:58:19 -0400

    Moved index.js to pages/ for Vercel routing fix

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>🚀 Welcome to Masxort!</h1>
-      <p>Built on Simplicity. Engineered to Evolve.</p>
-    </div>
-  );
-}
