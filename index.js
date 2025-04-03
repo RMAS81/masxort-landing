1f0dc3d6be85ef3bbf8f8959605aaadb2c4fccb9
RMAS81 <rmas@masxort.com>
2025-04-03 18:54:13 -0400

    Add homepage index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🚀 Welcome to Masxort!</h1>
+      <p>Built on Simplicity. Engineered to Evolve.</p>
+    </div>
+  );
+}
